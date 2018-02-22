8e77707d8054224422dc01d6707d2b35b8b6ba0d
Kalvin <kalvinlim11@gmail.com>
2018-02-21 23:35:05 -0600

    v2

--- src/test/java/AddTwo.java
+++ src/test/java/AddTwo.java
@@ -2,7 +2,6 @@ import linkedlist.LinkedListUtils;
 import linkedlist.ListNode;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class AddTwo {
@@ -19,11 +18,11 @@ public class AddTwo {
 
     @Test
     public void addUneven(){
-        ListNode one = LinkedListUtils.createList(9,9);
+        ListNode one = LinkedListUtils.createList(9,8);
         ListNode two = LinkedListUtils.createList(1);
 
         ListNode result = addTwoNumbers(one, two);
-        ListNode expected = LinkedListUtils.createList(0,0,1);
+        ListNode expected = LinkedListUtils.createList(0,9);
         assertTrue(LinkedListUtils.equalLinkLists(result, expected));
     }
 
@@ -85,7 +84,24 @@ public class AddTwo {
         }
 
         while(l1.next != null && l2.next == null){
+            int value = l1.val;
+            if(carry){
+                value++;
+            }
 
+            if(l2.val == 9){
+                carry = true;
+            } else {
+                carry = false;
+            }
+            value = value%10;
+
+            p.next = new ListNode(value);
+            p = p.next;
+
+            System.out.println(value);
+            l1 = l1.next;
+            l2 = l2.next;
         }
 
         if(l1.next==null && l2.next==null){
---
+++ src/test/java/AddTwoV2.java
@@ -0,0 +1,70 @@
+import linkedlist.LinkedListUtils;
+import linkedlist.ListNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AddTwoV2 {
+    @Test
+    public void addTwoSimple(){
+        ListNode one = new ListNode(5);
+        ListNode two = new ListNode(5);
+
+        ListNode expected = LinkedListUtils.createList(0,1);
+        ListNode result = addTwoNumbers(one, two);
+
+
+        //assertTrue(LinkedListUtils.equalLinkLists(expected, result));
+    }
+
+    @Test
+    public void addUneven(){
+        ListNode one = LinkedListUtils.createList(9,8);
+        ListNode two = LinkedListUtils.createList(1);
+
+        ListNode result = addTwoNumbers(one, two);
+        ListNode expected = LinkedListUtils.createList(0,9);
+        //assertTrue(LinkedListUtils.equalLinkLists(result, expected));
+    }
+
+    @Test
+    public void addTwoDoubleDigit(){
+        ListNode one = LinkedListUtils.createList(9,9);
+        ListNode two = LinkedListUtils.createList(9,9);
+
+        ListNode expected = LinkedListUtils.createList(8,9,8);
+        ListNode result = addTwoNumbers(one, two);
+
+        assertTrue(LinkedListUtils.equalLinkLists(expected, result));
+    }
+
+    public ListNode addTwov2(ListNode l1, ListNode l2){
+        while(l1.next != null && l2.next!=null){
+
+            System.out.println(l1.val + " + " +l2.val);
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        return null;
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        ListNode p = new ListNode(0);
+        ListNode head = p;
+        boolean carry = false;
+
+        while(l1 != null || l2 != null){
+            int val1  = l1 != null ? l1.val : 0;
+            int val2 = l2 != null ? l2.val : 0;
+            System.out.println(val1 + " + " + val2);
+
+            if(l1 != null) { l1 = l1.next; }
+            if(l2 != null) { l2 = l2.next; }
+        }
+
+        return head.next;
+    }
+
+
+
+}
