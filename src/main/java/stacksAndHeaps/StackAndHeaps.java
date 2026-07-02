6a9158dbfb9a0ef5bf33d696b68d8b199f548005
Kalvin <2porkchops@gmail.com>
2026-07-01 22:10:31 -0500

    next greater element

--- src/main/java/stacksAndHeaps/StackAndHeaps.java
+++ src/main/java/stacksAndHeaps/StackAndHeaps.java
@@ -1,9 +1,6 @@
 package stacksAndHeaps;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
 public class StackAndHeaps {
     public static int calculateDiscountedTotal(int[] prices) {
@@ -48,6 +45,32 @@ public class StackAndHeaps {
             stack.push(arr[i]);
         }
 
+        return result;
+    }
+
+    //Leetcode 496
+    //Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+    //Output: [-1,3,-1]
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[nums1.length];
+        
+        for(int i=nums2.length-1; i>=0; i--) {
+            while(!stack.isEmpty() && stack.peek() <= nums2[i]) {
+                stack.pop();
+            }
+
+            if(!stack.isEmpty()) {
+                map.put(nums2[i],stack.peek());
+            }
+
+            stack.push(nums2[i]);
+        }
+        System.out.println(map);
+        for(int i=0;i<nums1.length;i++) {
+            result[i] = map.getOrDefault(nums1[i], -1);
+        }
         return result;
     }
 
